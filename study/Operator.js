59659746ea25b6dae2eb27d37cd646ce2ab23892
kaeuhy <20212105@edu.hanbat.ac.kr>
2025-03-07 11:40:46 +0900

    Operator

---
+++ study/Operator.js
@@ -0,0 +1,11 @@
+var a = '1' + 2;
+var b = 1 + true;
+var c = 1 + false;
+var d = 1 + null;
+var e = +undefined;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
